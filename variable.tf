312d37da8a2c31bac91c394f7bf1fb82c19fde5f
bharatbalajid <bharatbalaji95@gmail.com>
2023-04-19 11:29:49 +0530

    cloud files

--- variable.tf
+++ variable.tf
@@ -6,14 +6,6 @@ variable "ingress_ports" {
   type    = list(number)
   default = [22, 8080]
 }
-variable "instance_type" {
-  type = map(any)
-  default = {
-    "us-east-1a" = "t2.micro"
-    "us-east-1b" = "t2.medium"
-    "us-east-1c" = "t2.large"
-  }
-}
 variable "az" {
   type    = list(string)
   default = ["us-east-1a", "us-east-1b", "us-east-1c"]
